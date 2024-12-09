4dba448fd06b67357859a18c8bf234eac5aa63a0
nycollasWildney <nycollas.wildney@gmail.com>
2024-12-08 21:35:15 -0300

    mais css

--- sistema_projeto_integrador/frontend/src/Componentes/componentesJS/Agenda.js
+++ sistema_projeto_integrador/frontend/src/Componentes/componentesJS/Agenda.js
@@ -24,6 +24,7 @@ const Agenda = () => {
       <div className="agenda-filtro">
         <label htmlFor="data">Selecione uma data:</label>
         <input
+          className="agenda-data"
           type="date"
           id="data"
           value={data}
